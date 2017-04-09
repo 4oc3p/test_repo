6fa21a9a8bc47d7a7c3982bbe6a9612027a10495
4oc3p <l4oc3pl@gmail.com>
2017-04-09 23:46:40 +0300

    first commit

---
+++ src/main/java/com/HelloWorld.java
@@ -0,0 +1,10 @@
+package com;
+
+/**
+ * Created by 4oc3p on 09.04.2017. repo
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
